bbd05ae7464e96095b809ac85e4494abc83164ed
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-04-28 00:23:22 +0500

    upd

--- src/pages/Authorization/ui/Authorization.tsx
+++ src/pages/Authorization/ui/Authorization.tsx
@@ -15,7 +15,11 @@ const Authorization: React.FC = () => {
         password
       }, {
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          "Access-Control-Allow-Credentials": 'true',
+          "Access-Control-Allow-Origin": '*',
+          "Access-Control-Allow-Methods": 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          "Access-Control-Allow-Headers": 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
         }
       });
       console.log('Успешная авторизация', response.data);
